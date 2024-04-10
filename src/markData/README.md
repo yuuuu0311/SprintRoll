40c953500f2a7b12bcd668aa466df12038ddddc4
yuuuu0311 <a26430803@gmail.com>
2024-04-10 15:49:49 +0800

    build: setup alias path

---
+++ src/markData/README.md
@@ -0,0 +1,10 @@
+### user
+
+```js
+{
+    domain: Number, //0: frontend 1: backend
+    name: String,
+    role: Number, //0: develop 1: manger
+    email: String,
+}
+```
---
+++ src/utilities/index.tsx
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD47I7lV3NBKsqnYSIMTKf64HtPpi5sBfo",
+    authDomain: "sprintroll-9c8c2.firebaseapp.com",
+    projectId: "sprintroll-9c8c2",
+    storageBucket: "sprintroll-9c8c2.appspot.com",
+    messagingSenderId: "772872341958",
+    appId: "1:772872341958:web:b21f99865028bf39c48ac7",
+};
+
+export const click = () => {
+    console.log("click");
+};
