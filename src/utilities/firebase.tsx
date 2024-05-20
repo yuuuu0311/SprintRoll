0490d17e408982023f19b8d036671d8cac9437fe
yuuuu0311 <a26430803@gmail.com>
2024-05-20 14:38:37 +0800

    build: add env variable

--- src/utilities/firebase.tsx
+++ src/utilities/firebase.tsx
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD47I7lV3NBKsqnYSIMTKf64HtPpi5sBfo",
-    authDomain: "sprintroll-9c8c2.firebaseapp.com",
-    projectId: "sprintroll-9c8c2",
-    storageBucket: "sprintroll-9c8c2.appspot.com",
-    messagingSenderId: "772872341958",
-    appId: "1:772872341958:web:b21f99865028bf39c48ac7",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
