f3b1eefca7397befc17f634a8970fd673bcc24d0
yuuuu0311 <a26430803@gmail.com>
2024-05-20 09:30:31 +0800

    fix: import getProgressPercentage from uitilties

--- src/pages/DashBoardPage/SprintPanel.tsx
+++ src/pages/DashBoardPage/SprintPanel.tsx
@@ -25,14 +25,6 @@ import {
     getProgressPercentage,
 } from "@/utilities";
 
-export const getProgressPercentage = (sprintTickets: TicketFace[]) => {
-    const havingStatus = sprintTickets.filter((ticket) => ticket.status !== -1);
-
-    return havingStatus.length === 0 && sprintTickets.length === 0
-        ? 0
-        : Math.floor((havingStatus.length / sprintTickets.length) * 100);
-};
-
 const getDateString = (date: Timestamp) =>
     new Date(
         date.seconds * 1000 + date.nanoseconds / 1000000
