929e74f7dd6a847ec9831f2dc4812d5f4329ed5b
yuuuu0311 <a26430803@gmail.com>
2024-05-22 17:38:17 +0800

    fix: update develop api_key

--- src/test/getProgressPercentage.test.tsx
+++ src/test/getProgressPercentage.test.tsx
@@ -38,20 +38,6 @@ const mockTicketArray_zero = [
         domain: "frontend",
         // inSprint: null,
         label: { feature: true },
-        order: 2,
-        id: 2,
-
-        status: 1,
-        ticketID: "ngIOhD9eoAdZBKfKG3DD",
-        title: "廣告區塊切版2",
-    },
-    {
-        collectionID: "miYoJELBhHuMvL78jmAE",
-        createdTime: { seconds: 1715666411, nanoseconds: 11000000 },
-        description: "5/14收到UI Team的Figma檔案",
-        domain: "frontend",
-        // inSprint: null,
-        label: { feature: true },
         order: 3,
         id: 3,
 
